0df4679728bfe854b76d0131c3a73a9c262b398e
simplast <840004959@qq.com>
2018-09-20 14:38:27 +0800

    new_homePage

---
+++ demo/node_1/server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const url = require('url');
+
+function start() {
+    http.createServer(function(request, response) {
+        const pathname = url.parse(request.url).pathname;
+        console.log('Request for' + pathname + 'received.');
+        response.writeHead(200, { "Content-type": "text/plain" });
+        response.write("Hello World");
+        response.end();
+    }).listen(8765);
+}
+
+exports.start = start;
