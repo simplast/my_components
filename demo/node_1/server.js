f5c8128dbf63c746e6ac0a276de34c29bb8f17ee
simplast <840004959@qq.com>
2018-09-21 09:21:53 +0800

    node_1

--- demo/node_1/server.js
+++ demo/node_1/server.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const url = require('url');
 
-function start() {
+function start(route, handle) {
     http.createServer(function(request, response) {
         const pathname = url.parse(request.url).pathname;
         console.log('Request for' + pathname + 'received.');
+
+        route(handle, pathname);
+
         response.writeHead(200, { "Content-type": "text/plain" });
         response.write("Hello World");
         response.end();
