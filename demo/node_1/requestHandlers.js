a3e862d8252a76cafd8b2e381eba5f8a7dd1e8c1
simplast <840004959@qq.com>
2018-09-21 15:09:04 +0800

    DETAILS page

--- demo/node_1/requestHandlers.js
+++ demo/node_1/requestHandlers.js
@@ -1,5 +1,19 @@
 function start() {  
     console.log('Request handler "start" was called.');
+
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+        '</html>';
+    
 }
 function upload() {  
     console.log('Request handler "upload" was called.');
